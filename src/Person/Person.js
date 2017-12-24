870269231ef938d4e065ecc96b2c735703322aea
vishnu vinayan <vishnuv336.com>
2017-12-24 18:16:58 +0530

    worked on css pseudo elements

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,7 @@ const person = (props) => {
 
   return (
     <div className={classes.Person}>
-      <p onClick={props.click}>I am {props.name}. I am {props.age} years old</p>
+      <p className={classes.para} onClick={props.click}>I am {props.name}. I am {props.age} years old</p>
       <p>{props.children}</p>
       <input onChange={props.changed} type="text" value={props.name}/>
     </div>
