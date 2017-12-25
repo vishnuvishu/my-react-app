e93d5e0135185782b9e45bea8107a71fc2d34037
vishnu vinayan <vishnuv336.com>
2017-12-25 11:08:00 +0530

    error boundary added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,12 @@ import classes from './Person.css'
 
 const person = (props) => {
 
+  // const rand = Math.random();
+  //
+  // if(rand > 0.7){
+  //   throw new Error('something went error');
+  // }
+
   return (
     <div className={classes.Person}>
       <p className={classes.para} onClick={props.click}>I am {props.name}. I am {props.age} years old</p>
