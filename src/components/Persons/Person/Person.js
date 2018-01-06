421234144750caabfeed350f5cd7c9c0ebda3f38
vishnu vinayan <vishnuv336.com>
2018-01-06 22:08:26 +0530

    understanding higher order components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import classes from './Person.css'
+import WithClass from '../../../hoc/WithClass'
 
 class Person extends Component{
 
@@ -19,11 +20,11 @@ class Person extends Component{
   render(){
     console.log('[Person.js] render()');
     return (
-      <div className={classes.Person}>
+      <WithClass classes={classes.Person}>
         <p className={classes.para} onClick={this.props.click}>I am {this.props.name}. I am {this.props.age} years old</p>
         <p>{this.props.children}</p>
         <input onChange={this.props.changed} type="text" value={this.props.name}/>
-      </div>
+      </WithClass>
     )
     // return [
     //   <p key="1" onClick={this.props.click}>I am {this.props.name}. I am {this.props.age} years old</p>,
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
 
@@ -94,7 +95,7 @@ deletePersonHandler = (personIndex) => {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes = {classes.App}>
         <button onClick={()=>{this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle = {this.props.title}
@@ -103,7 +104,7 @@ deletePersonHandler = (personIndex) => {
           clicked = {this.togglePersonHandler}
         />
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
