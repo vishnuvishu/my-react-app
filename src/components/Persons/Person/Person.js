762bac1759741647d87c131477c889a1ca7ddbf5
vishnu vinayan <vishnuv336.com>
2018-01-06 23:01:43 +0530

    validating props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import classes from './Person.css'
 import withClass from '../../../hoc/withClass'
 import Aux from '../../../hoc/Aux';
+import PropTypes from 'prop-types';
 
 class Person extends Component{
 
@@ -33,6 +34,13 @@ class Person extends Component{
     //   <input key="3" onChange={this.props.changed} type="text" value={this.props.name}/>
     // ]
   }
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 }
 
 export default withClass(Person, classes.Person);
